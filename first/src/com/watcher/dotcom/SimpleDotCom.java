3039562ccb0fd5d498600c802477089385b5ddce
over-watch <253354686@qq.com>
2016-09-11 20:41:10 +0800

    second commit，the dotcom attack game

--- first/src/com/watcher/dotcom/SimpleDotCom.java
+++ first/src/com/watcher/dotcom/SimpleDotCom.java
@@ -1,5 +1,27 @@
 package com.watcher.dotcom;
 
 public class SimpleDotCom {
+	private int[] loctionCells;//the attack 
+	private int numOfHits=0;//number of hit
 	
+	public void setLoctionCells(int[] loctionCells) {
+		this.loctionCells = loctionCells;
+	}
+	
+	public String checkYourself(String stringGuess){
+		int guess=Integer.parseInt(stringGuess);
+		String result="miss";
+		for(int cell:loctionCells){
+			if(guess==cell){
+				result="hit";
+				numOfHits++;
+				break;
+			}
+		}
+		if(numOfHits==loctionCells.length){
+			result="kill";
+		}
+		System.out.println(result);
+		return result;
+	}
 }
